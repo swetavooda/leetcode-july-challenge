60415553729e08d1bbe36f278f214e8df3e0306d
Sweta <sweta.vooda@gmail.com>
2020-07-03 20:58:14 +0530

    added day 2 solution(tree level order traversal)

---
+++ day_02.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public List<List<Integer>> levelOrderBottom(TreeNode root)
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null)
+            return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            int s=q.size();
+            List<Integer> l= new ArrayList<>();
+            for(int i=0;i<s;i++)
+            {
+                TreeNode c= q.poll();
+                l.add(c.val);
+                if(c.left!=null)
+                    q.offer(c.left);
+                if(c.right!=null)
+                    q.offer(c.right);
+                
+            }
+            ans.add(0,l);
+        }
+        return ans;
+    }
+}
