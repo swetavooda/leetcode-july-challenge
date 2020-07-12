b271668fcf352e1fa4c75973629742f96b0c6b8b
Sweta <sweta.vooda@gmail.com>
2020-07-12 21:52:53 +0530

    added weekly contest sol

---
+++ weekly-contest-197-1.py
@@ -0,0 +1,10 @@
+# prblem link: https://leetcode.com/problems/number-of-good-pairs/
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans=0
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]==nums[j]):
+                    ans+=1
+        return ans
+        
